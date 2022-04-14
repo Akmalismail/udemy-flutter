042b3a1d670b60365cbd51c175516beb3afa8c4d
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2022-04-14 11:06:10 +0800

    24. Building an App From Scratch

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  print("Hello World");
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello World!'),
+    );
+  }
+}
