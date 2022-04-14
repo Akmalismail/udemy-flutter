726541a5a4702b1555dff3a28888b152523986f7
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2022-04-14 11:14:08 +0800

    25. Running the App on an Emulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  print("Hello World");
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text('Hello World!'),
