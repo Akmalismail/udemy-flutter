f0e324f82c42b782beae71228e45c055b2688af2
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2022-04-21 15:48:09 +0800

    84. Adding Text Input Widgets (TextField)

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,6 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
@@ -47,6 +46,34 @@ class MyHomePage extends StatelessWidget {
               color: Colors.blue,
               child: Text('Chart here'),
               elevation: 5,
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Title',
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Amount',
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () => {},
+                    child: Text('Add Transaction'),
+                    style: TextButton.styleFrom(
+                      primary: Colors.purple,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           Column(
