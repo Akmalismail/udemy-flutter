5d23809e6ae95df6810ce1657633de808145780e
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2022-05-10 09:52:39 +0800

    165. Diving Deeper into Named Routes

--- lib/main.dart
+++ lib/main.dart
@@ -33,9 +33,11 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
+      initialRoute: '/', // default is '/'
       routes: {
-        '/category-meals': (context) => CategoryMealsScreen(),
+        '/': (_) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (_) => CategoryMealsScreen(),
       },
     );
   }
