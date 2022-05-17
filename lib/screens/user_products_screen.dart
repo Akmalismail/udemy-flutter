8209a569ea2059ba6466badfb0b9273c716220df
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2022-05-17 10:06:00 +0800

    220. Adding a "Manage Products" Page

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
+import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final products = Provider.of<ProductsProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: products.items.length,
+          itemBuilder: (_, index) => Column(
+            children: [
+              UserProductItem(
+                products.items[index].title,
+                products.items[index].imageUrl,
+              ),
+              Divider()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
