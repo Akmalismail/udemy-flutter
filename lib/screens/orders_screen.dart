61c7df049e89451dd90472f8d60e28b41c31053d
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2022-05-25 10:59:11 +0800

    259. Using the "FutureBuilder" Widget & Improving the Code

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,50 +5,82 @@ import '../widgets/app_drawer.dart';
 import '../widgets/order_item.dart';
 import '../providers/orders_provider.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // switch it up to use various methods.
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<OrdersProvider>(context, listen: false)
-          .fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final ordersProvider = Provider.of<OrdersProvider>(context);
+    print('building orders');
+    // commented to avoid infinite loop
+    // final ordersProvider = Provider.of<OrdersProvider>(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: ordersProvider.orders.length,
+      body: FutureBuilder(
+        /**
+         * if there is another state changing logic (such as presenting dialog) that rebuilds this widget,
+         * the .fetchAndSetOrders() will run again eventhough orders were not affected whatsover
+         * which is a waste of resources.
+         *
+         * Incase that happens, turn this widget to a [StatefulWidget] and then implement like this:
+         * 
+         * ```
+         * class _OrdersScreenState extends State<OrdersScreen> {
+         *  Future _ordersFuture;
+         * 
+         *  Future _obtainOrdersFuture() {
+         *    return Provider.of<OrdersProvider>(context, listen: false).fetchAndSetOrders();
+         *  }
+         * 
+         *  @override
+         *  void initState() {
+         *    _ordersFuture = _obtainOrdersFuture();
+         *    super.initState();
+         *  }
+         * 
+         *  @override
+         *  Widget build(BuildContext context) {
+         *    return Scaffold(
+         *      ...
+         *      body: FutureBuilder(
+         *        ...
+         *        future: _ordersFuture, <--- this ensures no new [Future] of .fetchAndSetOrders() are created.
+         *        ...
+         *      ),
+         *      ...
+         *    )
+         *  }
+         * }
+         * ```
+         */
+        future: Provider.of<OrdersProvider>(context, listen: false)
+            .fetchAndSetOrders(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.error != null) {
+            // ...
+            // do error handling here
+            return Center(
+              child: Text('An error occured!'),
+            );
+          }
+
+          return Consumer<OrdersProvider>(
+            builder: (context, orderData, child) => ListView.builder(
+              itemCount: orderData.orders.length,
               itemBuilder: (context, index) => OrderItem(
-                ordersProvider.orders[index],
+                orderData.orders[index],
               ),
             ),
+          );
+        },
+      ),
     );
   }
 }
