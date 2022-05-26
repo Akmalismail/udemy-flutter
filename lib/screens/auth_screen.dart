188235c2a65a6b941e499693dfe92478cebc2a05
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2022-05-26 14:54:14 +0800

    283. Using the "AnimatedBuilder" Widget

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -121,7 +121,7 @@ class _AuthCardState extends State<AuthCard>
       ),
     );
 
-    _heightAnimation.addListener(() => setState(() {}));
+    // _heightAnimation.addListener(() => setState(() {}));
   }
 
   @override
@@ -129,7 +129,7 @@ class _AuthCardState extends State<AuthCard>
     super.dispose();
 
     _controller.dispose();
-    _heightAnimation.removeListener(() => setState(() {}));
+    // _heightAnimation.removeListener(() => setState(() {}));
   }
 
   void _showErrorDialog(String message) {
@@ -224,14 +224,18 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints:
-            // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-            BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, ch) => Container(
+          // height: _authMode == AuthMode.Signup ? 320 : 260,
+          height: _heightAnimation.value.height,
+          constraints:
+              // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+              BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: ch,
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
