f2afaed72317c23cefed0ea1518615e8a4724b22
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2022-06-07 12:24:02 +0800

    319. Getting Started with Flutter & the Firebase SDK

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -8,8 +9,21 @@ class ChatScreen extends StatelessWidget {
         itemCount: 10,
         itemBuilder: (context, index) => Container(
           padding: const EdgeInsets.all(8),
-          child: Text('This works!'),
+          child: const Text('This works!'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/KzKFUQCTohS7vkYQ93wP/messages')
+              .snapshots()
+              .listen((data) {
+            data.docs.forEach((document) {
+              print(document['text']);
+            });
+          });
+        },
       ),
     );
   }
