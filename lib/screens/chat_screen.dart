46dee520c80d59e7291b48c7dcb9e246d5fdfe6f
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2022-06-09 10:52:31 +0800

    349. iOS & Push Notifications

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,9 +1,26 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/widgets/chat/messages.dart';
 import 'package:flutter_complete_guide/widgets/chat/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    messaging.requestPermission().then((settings) {
+      print(
+          'User granted permission: ${settings.authorizationStatus == AuthorizationStatus.authorized}');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
